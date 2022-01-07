605834e266e8baff615d21ebc6c1a924c2be55dc
Kanchuking1 <kanchuking1@gmail.com>
2022-01-07 14:24:22 +0530

    Few Sections Fixed

---
+++ Frontend/src/Pages/Home/IntroSection.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export class IntroSection extends Component {
+    content = [{
+        firstLine: "What ",
+        secondLine: "are we?",
+        description: "Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray."
+    }]
+    render() {
+        return (
+            <div>
+                {this.content.map ((section) => (
+                    <div>
+                        {console.log(section)}
+                        <section class="font-encode dark-theme border-t border-gray-200">
+                            <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                                <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                                    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white font-extrabold lg:text-7xl">{section.firstLine}
+                                        <br class="hidden lg:inline-block" />{section.secondLine}
+                                    </h1>
+                                    <p class="mb-8 leading-relaxed">{section.description}</p>
+                                    <div class="flex justify-center">
+                                        {/* <button class="ml-4 inline-flex text-gray-900 bg-white border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">Button</button> */}
+                                    </div>
+                                </div>
+                                <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                                    <img class="object-cover object-center rounded" alt="hero" src="/assets/WebsiteRenders/AllNoBG.png" />
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                ))}
+            </div>
+            
+        )
+    }
+}
+
+export default IntroSection
--- Frontend/src/Pages/Home/index.js
+++ Frontend/src/Pages/Home/index.js
@@ -1,24 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
+import HeroSection from './HeroSection'
+import IntroSection from './IntroSection'
+import WorkingSection from './WorkingSection'
+import FooterSection from './FooterSection'
 
 export default function() {
     return (<>
-    <div className={`flex mx-auto`} style={{ height: "90vh" }}>
+    {/* <div className={`flex mx-auto`} style={{ height: "90vh" }}>
         <div class="flex flex-col items-center dark-theme my-auto mx-auto">
-            <img src='/assets/WebsiteRenders/BlueVRedNoBG.png' class='side-img' alt='IMG TBD'/></div>    
-        </div>
-    <div className={`flex mx-auto w-4/5`} style={{ height: "100vh" }}>
-        <div class="flex flex-col dark-theme w-1/2 my-auto mx-auto p-6 -m-16 rounded-lg shadow-lg font-encode text-left">
-            <div class="font-extrabold text-7xl text-white">What are we?</div>
-            <br />
-            <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut finibus tempor nisi, vulputate aliquam orci molestie quis.
-            </div>
-        </div>
-        <div class="flex flex-col dark-theme w-1/2 my-auto mx-auto p-6 -m-16 rounded-lg font-encode">
-            <div class="mx-auto my-auto"><img src='/assets/WebsiteRenders/Render1_no_bg.png' class='side-img' alt='IMG TBD'/></div>
-        </div>
-    </div>
-    <div className={`flex mx-auto w-4/5`} style={{ height: "100vh" }}>
+            <img src='/assets/WebsiteRenders/BlueVRedNoBG.png' class='side-img' alt='IMG TBD'/>
+        </div>    
+    </div> */}
+    <HeroSection />
+    <IntroSection />
+    <WorkingSection />
+    {/* <div className={`flex mx-auto w-4/5`} style={{ height: "100vh" }}>
         <div class="flex flex-col dark-theme w-1/2 my-auto mx-auto p-6 -m-16 rounded-lg font-encode">
             <div class="mx-auto my-auto"><img src='/assets/WebsiteRenders/Render1_no_bg.png' class='side-img' alt='IMG TBD'/></div>
         </div>
@@ -29,7 +25,7 @@ export default function() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut finibus tempor nisi, vulputate aliquam orci molestie quis.
             </div>
         </div>
-    </div>
+    </div> */}
     <div className={`flex mx-auto w-4/5`} style={{ height: "100vh" }}>
         <div class="flex flex-col dark-theme w-1/2 my-auto mx-auto p-6 -m-16 rounded-lg shadow-lg font-encode text-left">
             <div class="font-extrabold text-7xl text-white">Roadmap</div>
@@ -50,5 +46,6 @@ export default function() {
             </div>
         </div>
     </div>
+    <FooterSection />
 </>)
 }
